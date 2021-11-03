950fd1075d223ec8abf46fe98125f090ad04e601
Jason <jasonp670@gmail.com>
2021-11-02 22:13:34 -0600

    add LoadingBox and MessageBox and css

---
+++ front-end/src/components/LoadingBox.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function LoadingBox() {
+  return (
+    <div className="row center" style={{ height: "100%" }}>
+      <div className="loader"></div>
+    </div>
+  );
+}
