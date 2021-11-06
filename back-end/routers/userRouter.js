c2ac7b06b80cf89cb27ff9f1cde7c0fe0bfd277b
Jason <jasonp670@gmail.com>
2021-11-05 22:44:29 -0600

    see if new user already exists

--- back-end/routers/userRouter.js
+++ back-end/routers/userRouter.js
@@ -44,20 +44,27 @@ userRouter.post(
 userRouter.post(
   "/register",
   expressAsyncHandler(async (req, res) => {
-    console.log("hey");
-    const user = await User.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: bcrypt.hashSync(req.body.password, 8),
-    });
-    console.log("New users id: ", user.id);
-    res.send({
-      id: user.id,
-      name: user.name,
-      email: user.email,
-      isAdmin: user.isAdmin,
-      token: generateToken(user),
-    });
+    const checkExist = await User.findOne({ where: { email: req.body.email } });
+    console.log(checkExist);
+    if (checkExist) {
+      res.status(400).send({ message: "Entry already exists with that email" });
+    } else {
+      console.log("made it through");
+      const user = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: bcrypt.hashSync(req.body.password, 8),
+      });
+      console.log("New users id: ", user.id);
+      console.log(user);
+      res.send({
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        isAdmin: user.isAdmin,
+        token: generateToken(user),
+      });
+    }
   })
 );
 
