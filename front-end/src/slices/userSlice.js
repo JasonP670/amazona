e6ef5f623e8efe408b4121491d9bf012439752f8
Jason <jasonp670@gmail.com>
2021-11-22 11:32:23 -0700

    add get user details

--- front-end/src/slices/userSlice.js
+++ front-end/src/slices/userSlice.js
@@ -55,17 +55,29 @@ export const createAddress = createAsyncThunk(
       postalCode,
       country,
     };
-    console.log(token);
-    console.log(body);
 
     const { data } = await Axios.post(`/api/users/address/`, body, {
       headers: {
         authorization: `Bearer ${token}`,
       },
     });
-    console.log(data);
   }
 );
+
+export const detailsUser = createAsyncThunk("user/detailsUser", async (id) => {
+  try {
+    console.log(id);
+    const { data } = await Axios.get(`/api/users/${id}`);
+    console.log(data);
+    return data;
+  } catch (error) {
+    const message =
+      error.response && error.response.data.message
+        ? error.response.data.message
+        : error.message;
+    return message;
+  }
+});
 
 const options = {
   name: "user",
@@ -73,6 +85,7 @@ const options = {
     userData: localStorage.getItem("user")
       ? JSON.parse(localStorage.getItem("user"))
       : null,
+    userDetails: {},
     addresses: [],
     error: false,
     loading: false,
@@ -125,6 +138,18 @@ const options = {
       state.error = action.payload;
       state.loading = false;
     },
+    [detailsUser.pending]: (state, action) => {
+      state.loading = true;
+    },
+    [detailsUser.fulfilled]: (state, action) => {
+      console.log(action.payload);
+      state.loading = false;
+      state.userDetails = action.payload;
+    },
+    [detailsUser.rejected]: (state, action) => {
+      state.error = true;
+      state.loading = false;
+    },
   },
 };
 
@@ -134,4 +159,5 @@ export const selectUserData = (state) => state.user.userData;
 export const selectUserAddress = (state) => state.user.addresses;
 export const selectUserLoading = (state) => state.user.loading;
 export const selectUserError = (state) => state.user.error;
+export const selectUserDetails = (state) => state.user.userDetails;
 export const { signout } = userSlice.actions;
